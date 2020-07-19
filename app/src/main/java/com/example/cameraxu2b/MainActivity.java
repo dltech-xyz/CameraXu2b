415aa1dcae111d7054676524860884b29881678d
stevenjokes <a867907127@gmail.com>
2020-07-19 21:13:51 +0800

    add - to rotationDgr

--- app/src/main/java/com/example/cameraxu2b/MainActivity.java
+++ app/src/main/java/com/example/cameraxu2b/MainActivity.java
@@ -154,13 +154,13 @@ public class MainActivity extends AppCompatActivity {
                 rotationDgr = 0;
                 break;
             case Surface.ROTATION_90:
-                rotationDgr = 90;
+                rotationDgr = -90;
                 break;
             case Surface.ROTATION_180:
-                rotationDgr = 180;
+                rotationDgr = -180;
                 break;
             case Surface.ROTATION_270:
-                rotationDgr = 270;
+                rotationDgr = -270;
                 break;
             default:
                 return;
